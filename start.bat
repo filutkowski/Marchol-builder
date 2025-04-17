7c5b52faee52b3b8a525f3b2f56499e672ccf884
Michał Filutkowski <filutkowski@gmail.com>
2025-04-17 06:40:22 +0200

    5.6
    Signed-off-by: Michał Filutkowski <filutkowski@gmail.com>

--- start.bat
+++ start.bat
@@ -5,7 +5,9 @@ systeminfo
 call .\PGit\bin\git.exe clone https://github.com/filutkowski/Marchol.git
 cd Marchol
 call ..\node\npm.cmd install
+call robocopy ./src ./dist /E
+call npx tsc
 call ..\node\npx.cmd electron-builder
 cd pack
 start installer.exe
-exit
+pause
